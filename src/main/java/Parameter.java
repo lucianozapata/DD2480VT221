62a58ef12c96dd50a7118950d629bd66bf203bfa
Luciano <luciano.zapata@mentimeter.com>
2022-01-27 23:27:11 +0100

    fix: Implemented so that constructor creates defaultvalues 0 for each parameter

--- src/main/java/Parameter.java
+++ src/main/java/Parameter.java
@@ -24,33 +24,28 @@ public class Parameters {
 
 
 
-    // Contructor for all the parameters
-    public Parameters(double length1, double radius1, double epsilon, double area1,
-    int q_pts, int quads, double dist, int n_pts, int k_pts, int a_pts, int b_pts, int c_pts, int d_pts,
-    int e_pts, inte f_pts, int g_pts, double length2, double radius2 double area2) {
-
-        this.LENGTH1 = length1;
-        this.RADIUS1 = radius1;
-        this.EPSILON = epsilon;
-        this.AREA1 = area1;
-        this.Q_PTS = q_pts;
-        this.QUADS = quads;
-        this.DIST = dist;
-        this.N_PTS = n_pts;
-        this.K_PTS = k_pts;
-        this.A_PTS = a_pts;
-        this.B_PTS = b_pts;
-        this.C_PTS = c_pts;
-        this.D_PTS = d_pts;
-        this.E_PTS = e_pts;
-        this.F_PTS = f_pts;
-        this.G_PTS = g_pts;
-        this.LENGTH2 = length2;
-        this. RADIUS2 = radius2;
-        this.AREA2 = area2;
+    // Initializing all the parameters to ZERO when creating the PARAMETER-object.
+    public Parameters() {
+        this.LENGTH1 = 0;
+        this.RADIUS1= 0;
+        this.EPSILON= 0;
+        this.AREA1= 0;
+        this.Q_PTS= 0;
+        this.QUADS= 0;
+        this.DIST= 0;
+        this.N_PTS= 0;
+        this.K_PTS= 0;
+        this.A_PTS= 0;
+        this.B_PTS= 0;
+        this,.C_PTS= 0;
+        this.D_PTS= 0;
+        this.E_PTS= 0;
+        this.F_PTS = 0;
+        this.G_PTS= 0;
+        this.LENGTH2= 0;
+        this.RADIUS2= 0;
+        this.AREA2= 0;
+        
 
     }
-
-
-
 }
