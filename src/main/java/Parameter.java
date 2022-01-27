85257262fb73049d6220de8c1186c90d1c703265
Luciano <luciano.zapata@mentimeter.com>
2022-01-27 23:17:17 +0100

    feat: Created a parameterClass which holds all the parameters for LIC's

---
+++ src/main/java/Parameter.java
@@ -0,0 +1,56 @@
+public class Parameters {
+    /**
+        All parameters available.
+     */
+    double LENGTH1;
+    double RADIUS1;
+    double EPSILON;
+    double AREA1;
+    int Q_PTS;
+    int QUADS;
+    double DIST;
+    int N_PTS;
+    int K_PTS;
+    int A_PTS;
+    int B_PTS;
+    int C_PTS;
+    int D_PTS;
+    int E_PTS;
+    int F_PTS;
+    int G_PTS;
+    double LENGTH2;
+    double RADIUS2;
+    double AREA2;
+
+
+
+    // Contructor for all the parameters
+    public Parameters(double length1, double radius1, double epsilon, double area1,
+    int q_pts, int quads, double dist, int n_pts, int k_pts, int a_pts, int b_pts, int c_pts, int d_pts,
+    int e_pts, inte f_pts, int g_pts, double length2, double radius2 double area2) {
+
+        this.LENGTH1 = length1;
+        this.RADIUS1 = radius1;
+        this.EPSILON = epsilon;
+        this.AREA1 = area1;
+        this.Q_PTS = q_pts;
+        this.QUADS = quads;
+        this.DIST = dist;
+        this.N_PTS = n_pts;
+        this.K_PTS = k_pts;
+        this.A_PTS = a_pts;
+        this.B_PTS = b_pts;
+        this.C_PTS = c_pts;
+        this.D_PTS = d_pts;
+        this.E_PTS = e_pts;
+        this.F_PTS = f_pts;
+        this.G_PTS = g_pts;
+        this.LENGTH2 = length2;
+        this. RADIUS2 = radius2;
+        this.AREA2 = area2;
+
+    }
+
+
+
+}
