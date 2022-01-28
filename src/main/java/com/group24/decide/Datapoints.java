873f86463e744713bd66fc8698e89b017effb4f6
Pontus <persman@kth.se>
2022-01-28 20:52:42 +0100

    Made the whole project a package. Added some java skeleton files, such as a main class (Decide), LIC etc. #35
    Added a test class for LIC. Which should be used to test all the conditions.

---
+++ src/main/java/com/group24/decide/Datapoints.java
@@ -0,0 +1,30 @@
+package com.group24.decide;
+
+/**
+ * @author
+ */
+public class Datapoints {
+
+    double x;
+    double y;
+
+    /**
+     *
+     */
+    public Datapoints(double x, double y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+    /**
+     * Method for comparing datapoints.
+     *
+     * @param point1
+     * @param point2
+     * @return
+     */
+    public double compareDistance(Datapoints point1, Datapoints point2){
+        return 0;
+    }
+}
---
+++ src/main/java/com/group24/decide/Decide.java
@@ -0,0 +1,59 @@
+package com.group24.decide;
+
+/**
+ * Add description for javaDoc
+ *
+ * @author Pontus
+ */
+public class Decide {
+
+    static int NUMPOINTS = 5;
+
+    static LIC lic;
+
+    private static boolean[] CMV = new boolean[15];
+    private static boolean[][] PUM = new boolean[15][15];
+    private static boolean[] FUV = new boolean[15];
+
+    private static Parameter parameters;
+
+    static boolean LAUNCH;
+
+    enum CONNECTORS {
+        NOTUSED,
+        ORR,
+        ANDD
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+
+        Parameter parameters = new Parameter(0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0);
+
+        Datapoints[] Points = new Datapoints[NUMPOINTS];
+        Points[0] = new Datapoints(1, 2); // Input the data points
+
+        lic = new LIC(parameters);
+
+
+        // Input all values for LCM.
+        CONNECTORS[][] LCM = {
+                {CONNECTORS.ANDD, CONNECTORS.ANDD, CONNECTORS.ORR, CONNECTORS.ANDD, CONNECTORS.NOTUSED},
+                {CONNECTORS.ANDD, CONNECTORS.ANDD, CONNECTORS.ORR, CONNECTORS.ANDD, CONNECTORS.NOTUSED}
+        };
+
+        CMV = lic.runLICConditions(CMV.length); // Run the LIC conditions
+
+        LAUNCH = true;
+        for(int i=0;i<FUV.length;i++){
+            if( !FUV[i] ){
+                LAUNCH = false;
+                break;
+            }
+        }
+    }
+}
