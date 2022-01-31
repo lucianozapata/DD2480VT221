3137a20a6c154625b5450cd42b455a1133c4e505
Luciano <zapata@kth.se>
2022-01-31 16:38:00 +0100

    feat: Implemented LIC1 and tests #2

--- src/main/java/com/group24/decide/LIC.java
+++ src/main/java/com/group24/decide/LIC.java
@@ -60,13 +60,14 @@ public class LIC {
 
     /**
      *
-     * @return Return true if there exists at least one set
+     * Return true if there exists at least one set
      * of two consecutive data points that are a distance greater
      * than the length, LENGTH1, apart. (0 ≤ LENGTH1), else return false.
+     * 
+     * @return Return true if the condition is met, other
+     * returns false.
      */
         public boolean Condition0() {
-            // How many datapoints we are going to check.
-
             for (int index = 0; index < numberPoints-1; index++) {
                 // The distance between the two datapoints
                 double distance = Utility.calcEuclideanDistance(this.points[index], this.points[index + 1]);
@@ -78,9 +79,28 @@ public class LIC {
             }
             return false;
         }
+     /**
+     * There exists at least one set of three consecutive data points that cannot all be contained
+     * within or on a circle of radius RADIUS1. (0 ≤ RADIUS1)
+     *
+     * @return Return true if the condition is met, other
+     * returns false.
+     */
+        public boolean Condition1(){
+            if(parameters.RADIUS1 < 0){
+                return false;
+            }
+            if(numberPoints < 3){
+                return false;
+            }
 
-        public static boolean Condition1(){
-        return true;
+            for(int idx = 0; idx < numberPoints - 2; idx++){
+               double radius = Utility.findSmallestCircle(points[idx], points[idx+1], points[idx+2]);
+               if(radius > parameters.RADIUS1){
+                   return true;
+               }
+            }
+        return false;
     }
     /**
      *
